e9fdd6ecf28acfd2c84c5643a7dc96a1b79bf360
Fernanda Cacho <flemes19@gmail.com>
2022-06-24 17:30:37 -0400

    Exercícios adicionados

---
+++ Exercicio5.py
@@ -0,0 +1,12 @@
+#Programadora: Fernanda Lemes Cacho
+#Data 24/06/2022
+#Teste - exercício inversão de strings
+
+def inverte(palavra):
+  return palavra[::-1]
+
+
+palavra = input()
+print(inverte(palavra))
+
+
